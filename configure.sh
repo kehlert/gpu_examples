249d4eebb3dc01f56e26cbae447ae33c630da202
kurt <kehlert@wisc.edu>
2014-09-15 10:11:34 -0500

    The configure scripts now accepts a build type parameter (e.g. debug or release). The CMake files aren't yet entirely setup and tested yet, so the feature isn't functional.

--- configure.sh
+++ configure.sh
@@ -2,14 +2,49 @@
 
 set -e
 
-[ -d ./build ] || mkdir ./build
+usage() { echo "usage: $0 [-c] [-b build_type]"; exit 1; }
 
-if [ "$1" == "clean" ]; then
-	rm -rf build/*
+validBuildTypes=("DEBUG" "RELWITHDEBINFO")
+
+while getopts "b:c" opt; do
+    case "${opt}" in
+        c)
+            echo -e "-----\nCleaning the build directory."
+            rm -rf ./build/*
+            ;;
+        b)
+            valid=false
+            for validType in "${validBuildTypes[@]}"; do
+                if [ "$OPTARG" == "$validType" ]; then
+                    valid=true; break;
+                fi
+            done
+
+            if ! $valid; then
+                echo -n "ERROR: Build type not valid. Must be one of these: " 1>&2
+                for type in "${validBuildTypes[@]}"; do echo -n "$type " 1>&2; done
+                echo "" 1>&2;
+                exit 1
+            fi
+
+            buildType="${OPTARG}"
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+
+if [ -z "$buildType" ]; then
+    buildType="DEBUG"
 fi
 
+[ -d ./build ] || mkdir ./build
+
+echo -e "-----\nbuild type: $buildType\n-----"
+
 cd build
-cmake .. -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug 
+cmake .. -G "Unix Makefiles" -DCMAKE_BUILD_TYPE="${buildType}"
 cd ..
 
 #generate ctags
